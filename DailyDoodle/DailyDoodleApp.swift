9ba5c176d62f12b99f0bc0bed27a7681c2014106
Ieda Xavier <92699834+Sunlower@users.noreply.github.com>
2022-11-17 08:58:16 -0300

    Initial Commit

---
+++ DailyDoodle/DailyDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDoodleApp.swift
+//  DailyDoodle
+//
+//  Created by Ieda Xavier on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
