d0ff5b064291423ffbd3dee2f3ac4c1da739c93e
leticiadutra22-23 <leticiack.ldtra@gmail.com>
2022-11-24 08:49:55 -0300

    💥 fix: Error fixed and map item is selected by default

--- DailyDoodle/View/TabBar/CustomShape.swift
+++ DailyDoodle/View/TabBar/CustomShape.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct CustomShape: Shape {
     var xAxis: CGFloat
     var animatableData: CGFloat {
--- DailyDoodle/View/TabBar/TabBarView.swift
+++ DailyDoodle/View/TabBar/TabBarView.swift
@@ -3,20 +3,22 @@
 //  DailyDoodle
 //
 //  Created by Maria Letícia Dutra de Oliveira on 23/11/22.
-//
-
-import SwiftUI
 
 import SwiftUI
 
 //TAB BAR ANIMADA
 struct TabBar: View {
-    @State var selectedtab = "add"
+    @State var selectedtab = "map"
     @State var xAxis: CGFloat = 0
     @Namespace var animation
 
     var body: some View {
-        ZStack (alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+        ZStack (
+            alignment: Alignment(
+                horizontal: .center,
+                vertical: .bottom
+            )
+        ) {
             //MARK: Tabview atualiza a view de acordo com a tag
             TabView(selection: $selectedtab){
                 MapView()
@@ -31,9 +33,9 @@ struct TabBar: View {
             }
 
             //MARK: Configurações de imagens e botões para o que estiver selecionado
-            HStack(spacing: 0){
-                ForEach(icons, id: \.self){ text, image in
-                    GeometryReader {proxy in
+            HStack(spacing: 0) {
+                ForEach(icons, id: \.self){ image in
+                    GeometryReader { proxy in
                         Button(action: {
                             withAnimation(.spring()){
                                 selectedtab = image
@@ -50,19 +52,16 @@ struct TabBar: View {
                                 .background(Color(red: 255/255, green: 97/255, blue: 134/255).opacity(selectedtab == image  ? 1 : 0).clipShape(Circle()))
                                 .matchedGeometryEffect(id: image, in: animation)
                                 .offset(x: selectedtab == image ? (proxy.frame(in: .global).minX - proxy.frame(in: .global).midX) : 0, y: selectedtab == image  ? -54 : 0)
-                            Text(text)
-                                .foregroundColor(.black)
-                                .font(.title)
-
                     })
                         .onAppear(perform: {
-                            if image == icons.last{
+                            if image == icons[1] {
                                 xAxis = proxy.frame(in: .global).minX
                             }
                         })
-                    }.frame(width: 27, height: 35)
+                    }
+                    .frame(width: 27, height: 35)
 
-                    if image != icons.last{Spacer(minLength: 0)}
+                    if image != icons.last { Spacer(minLength: 0) }
                 }
             }
             .padding(.horizontal, 30)
